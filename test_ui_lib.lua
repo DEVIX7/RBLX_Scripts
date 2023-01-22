b9bd1fe6cad06b5dd4d1ba67635cd1085f9e9c19
DEVIX7 <78102400+DEVIX7@users.noreply.github.com>
2023-01-22 11:54:54 +0200

    Add files via upload

---
+++ test_ui_lib.lua
@@ -0,0 +1,105 @@
+--by devix7 & gpt chat
+    button.Size = UDim2.new(
+        button.Size = UDim2
+    
+        button.Position = UDim
+    local UI = {}
+    
+    function
+        local button = Instance.
+        button.Size = UDim2.
+        button.Position = UDim2.
+        button.
+       
+    
+        button.Mouse
+     UI.CreateButton(text, x, y, callback)
+        local button = Instance.new("TextButton")
+        button.Size = UDim2.new(0, 100, 0, 30)
+        button.Position = UDim2.new(x, y)
+        button.Text = text
+        button.Parent = game:GetService("CoreGui")
+        button.MouseButton1Click:Connect(callback)
+    end
+    
+    function UI.CreateLabel(text, x, y)
+        local label = Instance.
+        local label = Instance.
+    
+        local label = Instance
+    
+        local label = Inst
+    
+        local label =
+    
+        local label
+    
+        local
+    
+       
+    new("TextLabel")
+        label.Size = UDim2.
+        label.Size = UDim2.
+    
+        label.Size = UDim2
+    
+        label.Size = UDim
+    
+        label.Size = U
+    
+        label.Size =
+    
+        label.Size
+    
+        label.
+    
+        label
+    
+       
+    new(0, 100, 0, 30)
+        label.Position = UDim2.
+        label.Position = UDim2.
+    
+        label.Position = UDim2
+    
+        label.Position = UDim
+    
+        label.Position = U
+    
+        label.Position =
+    
+        label.Position
+    
+        label.
+    
+        label
+    
+       
+    new(x, y)
+        label.
+        label.
+    
+        label
+    
+       
+    Text = text
+        label.Parent = game:GetService(
+        label.Parent = game:GetService
+    
+        label.Parent = game:
+    
+        label.Parent = game
+    
+        label.Parent =
+    
+        label.Parent
+    
+        label.
+    
+        label
+    
+       
+    "CoreGui")
+    end
+    
+    return UI
