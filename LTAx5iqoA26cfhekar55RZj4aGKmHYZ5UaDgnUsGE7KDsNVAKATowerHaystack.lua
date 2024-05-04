a52b6867fb6a2b4357b7cb108c00b30834f326a8
DEVIX7 <georgiy120907@gmail.com>
2024-05-04 11:11:18 +0300

    + fix rotation modified:   LTAx5iqoA26cfhekar55RZj4aGKmHYZ5UaDgnUsGE7KDsNVAKATowerHaystack.lua

--- LTAx5iqoA26cfhekar55RZj4aGKmHYZ5UaDgnUsGE7KDsNVAKATowerHaystack.lua
+++ LTAx5iqoA26cfhekar55RZj4aGKmHYZ5UaDgnUsGE7KDsNVAKATowerHaystack.lua
@@ -1,84 +1,88 @@
 --[[ 
-	Stack V1 source
-	made by DEVIX7 
+	Stack V1.1 (Stable) source
+	made by DEVIX7
 ]]
 local v1 = loadstring(game:HttpGet("https://raw.githubusercontent.com/Sigmanic/ROBLOX/main/ModificationWallyUi", true))()
-local v2 = v1:CreateWindow("Stack V1")
+local v2 = v1:CreateWindow("Stack V1.1 (Stable)")
 local v3 = game:GetService("ReplicatedStorage").RemoteFunction
 local v4 = game.Players.LocalPlayer:GetMouse()
 local v5 = 1
 print("\n\n\t\t\t\tScript made by DEVIX7\n")
 local v6 = {}
 local v7 = {}
-for v8,v9 in next, v3:InvokeServer("Session", "Search", "Inventory.Troops") do
-    if v9.Equipped then
-        table.insert(v6, v8)
-        table.insert(v7, tostring(v8))
-    end
+for a1,a2 in next, v3:InvokeServer("Session", "Search", "Inventory.Troops") do
+   if a2.Equipped then
+       table.insert(v6, a1)
+       table.insert(v7, tostring(a1))
+   end
 end
 print(table.concat(v7, ", "))
-local v10 = v6[1]
-local v11 = v2:Toggle('Stacking', {flag = "stackToggle"})
-v2:Slider("Amount", {
-    precise = false,
-    default = 1,
-    min = 1,
-    max = 10,
-}, function(v12)
-    v5 = v12
+local v8 = v6[1]
+local v9 = v2:Toggle('Stacking', {flag = "stackToggle"})
+v2:Slider("Amount",
+   {
+       precise = false,
+       default = 1,
+       min = 1,
+       max = 10,
+   },
+function(a1)
+   v5 = a1
 end)
 v2:Section('\\/ UPGRADE \\/')
 v2:Button('Upgrade All', function()
-    for v13,v14 in pairs(game.Workspace.Towers:GetChildren()) do
-        if v14:WaitForChild("Owner").Value == game.Players.LocalPlayer.UserId then
-            v3:InvokeServer("Troops","Upgrade","Set",{["Troop"] = v14})
-            task.wait(0.15)
-        end
-    end
+for a1,a2 in pairs(game.Workspace.Towers:GetChildren()) do
+   if a2:WaitForChild("Owner").Value == game.Players.LocalPlayer.UserId then
+       v3:InvokeServer("Troops","Upgrade","Set",{["Troop"] = a2})
+       task.wait(0.15)
+   end
+end
 end)
 v2:Button("Upgrade Tower", function()
-    for v15,v16 in pairs(game.Workspace.Towers:GetChildren()) do
-        if v16:WaitForChild("Owner").Value == game.Players.LocalPlayer.UserId and v16:WaitForChild("Replicator"):GetAttribute("Type") == v10 then
-            v3:InvokeServer("Troops","Upgrade","Set",{["Troop"] = v16})
-            task.wait(0.15)
-        end
-    end
+   for a1,a2 in pairs(game.Workspace.Towers:GetChildren()) do
+       if a2:WaitForChild("Owner").Value == game.Players.LocalPlayer.UserId and a2:WaitForChild("Replicator"):GetAttribute("Type") == v8 then
+           v3:InvokeServer("Troops","Upgrade","Set",{["Troop"] = a2})
+           task.wait(0.15)
+       end
+   end
 end)
 v2:Dropdown("\\/ Set Tower \\/", {
-    location = _G;
-    flag = "troops";
-    list = v6;
-}, function(v17)
-    v10 = v17
+   location = _G;
+   flag = "troops";
+   list = v6;
+}, function(a1)
+   v8 = a1
 end)
 v2:Section('\\/ DANGER ZONE \\/')
 v2:Button('Sell All', function()
-    for v18,v19 in pairs(game.Workspace.Towers:GetChildren()) do
-        if v19:WaitForChild("Owner").Value == game.Players.LocalPlayer.UserId then
-            v3:InvokeServer("Troops","Sell",{["Troop"] = v19})
-            task.wait(0.15)
-        end
-    end
+   for a1, a2 in pairs(game.Workspace.Towers:GetChildren()) do
+       if a2:FindFirstChild("Owner") and a2.Owner.Value == game.Players.LocalPlayer.UserId then
+           v3:InvokeServer("Troops", "Sell", {["Troop"] = a2})
+           task.wait(0.15)
+       end
+   end
 end)
-local v20 = nil
-local v21
-v21 = hookmetamethod(game, "__namecall", function(v22, ...)
-    local v23 = {...}
-    local v24 = getnamecallmethod()
-    if v2.flags.stackToggle and v24 == "InvokeServer" and v22 == v3 and #v23 == 4 and v23[1] == "Troops" and v23[2] == "Place" then
-        spawn(function()
-            local v25 = v23[4]['Position']
-            local v26 = v25.Y
-            for v27 = 1, v5 do 
-                v20 = 1
-                v25 = Vector3.new(v25.X, v26 + (v27 - 1) * 5, v25.Z)
-                v3:InvokeServer(v23[1], v23[2], v23[3], {Rotation = CFrame.new(0,5,0,0,5,0,0,5,0,0,5,0), Position = v25}, true)
-                task.wait(0.15)
-            end
-            v20 = 0
-        end)
-        return nil
-    end
-    return v21(v22, ...)
+local v10 = nil
+local v11
+v11 = hookmetamethod(game, "__namecall", function(a1, ...)
+   local v12 = {...}
+   local v13 = getnamecallmethod()
+   if v9.flags.stackToggle and v13 == "InvokeServer" and a1 == v3 and #v12 == 4 and v12[1] == "Troops" and v12[2] == "Place" then
+       spawn(function()
+           local v14 = v12[4]['Position']
+           local v15 = v12[4]['Rotation']
+           print(v14 , v15)
+           local v16 = v14.Y
+           for a1 = 1, v5 do
+               v10 = 1
+               v14 = Vector3.new(v14.X, v16 + (a1 - 1) * 5, v14.Z)
+               v3:InvokeServer(v12[1], v12[2], v12[3], {Rotation = v15, Position = v14}, true)
+               task.wait(0.15)
+           end
+           v10 = 0
+       end)
+       return nil
+   end
+   return v11(a1, ...)
 end)
 v2:Section('made by DEVIX7')
