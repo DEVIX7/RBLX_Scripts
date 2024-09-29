af87511c3e36e9471a0c52505b39876d09664f30
Developer iX7 <78102400+DEVIX7@users.noreply.github.com>
2024-09-29 21:45:04 +0300

    patch

--- AutoLobby.lua
+++ AutoLobby.lua
@@ -1,44 +1,48 @@
---AutoLobby V2.5
+--AutoLobby V2.7
 --Script by devix7
 
-repeat
-    wait()
-until game:IsLoaded()
-
-local plr = game:GetService("Players").LocalPlayer
-local gameId = 5591597781
-local lobbyId = 3260590327
-
-print("github.com/devix7")
+repeat task.wait() until game:IsLoaded()
+print("\n\t\tgithub.com/devix7\n", "\t\tmade by DEVIX7\n", "\t\tAutoLobby V2.7")
 
 local function tpLobby()
     print("Teleporting to the lobby...")
-    wait(1)
-    game:GetService("TeleportService"):Teleport(lobbyId)
+    task.wait(1)
+    game:GetService("TeleportService"):Teleport(3260590327, game:GetService("Players").LocalPlayer)
 end
 
-if game.PlaceId == gameId then
-    local gameOver = plr.PlayerGui:WaitForChild("RoactGame"):WaitForChild("Rewards"):WaitForChild("content"):WaitForChild("gameOver")
-
-    gameOver:GetPropertyChangedSignal("Visible"):Connect(function()
-        if gameOver.Visible then
-            tpLobby()
-        end
+if game.PlaceId == 5591597781 then
+    task.spawn(function()
+        local gameOver = game:GetService("Players").LocalPlayer.PlayerGui:WaitForChild("ReactGame"):WaitForChild("Rewards"):WaitForChild("content"):WaitForChild("gameOver")
+        gameOver:GetPropertyChangedSignal("Visible"):Connect(function()
+            if gameOver.Visible == true then
+                local content = gameOver:FindFirstChild("content")
+                if content and content.Visible == true then
+                    tpLobby()
+                end
+            end
+        end)
     end)
 
-    game:GetService("CoreGui").RobloxPromptGui.promptOverlay.ChildAdded:Connect(function(element)
-        if element.Name == "ErrorPrompt" and element:FindFirstChild("MessageArea") and element.MessageArea:FindFirstChild("ErrorFrame") then
-            tpLobby()
-        end
+    task.spawn(function() 
+        game:GetService("CoreGui").RobloxPromptGui.promptOverlay.ChildAdded:Connect(function(obj)
+            if obj.Name == "ErrorPrompt" and obj:FindFirstChild("MessageArea") and obj.MessageArea:FindFirstChild("ErrorFrame") then
+                tpLobby()
+            end
+        end)
     end)
 
-elseif game.PlaceId == lobbyId then
-    game:GetService("CoreGui").RobloxPromptGui.promptOverlay.ChildAdded:Connect(function(element)
-        if element.Name == "ErrorPrompt" and element:FindFirstChild("MessageArea") and element.MessageArea:FindFirstChild("ErrorFrame") then
-            tpLobby()
-        end
+elseif game.PlaceId == 3260590327 then
+    task.spawn(function()
+        game:GetService("CoreGui").RobloxPromptGui.promptOverlay.ChildAdded:Connect(function(obj)
+            if obj.Name == "ErrorPrompt" and obj:FindFirstChild("MessageArea") and obj.MessageArea:FindFirstChild("ErrorFrame") then
+                tpLobby()
+            end
+        end)
     end)
-    print("Automatic reconnection after 5 minutes")
-    wait(300)
-    tpLobby()
+
+    task.spawn(function()
+        print("Automatic reconnection after 5 minutes")
+        task.wait(300)
+        tpLobby()
+    end)
 end
